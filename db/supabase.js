a658fc6eee4783cba5ecb92c69ca7d300e831a15
trynalearn-code <michaelshloush@gmail.com>
2026-07-30 16:32:01 +0300

    edited message in supabase db

--- db/supabase.js
+++ db/supabase.js
@@ -3,6 +3,6 @@ import "dotenv/config"
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_PUBLISHABLE_KEY)
 
-console.log("connected to supabase!")
+console.log("We've connected to supabase!")
 
 export default supabase
